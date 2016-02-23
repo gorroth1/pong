624aed0e82a11972c25e857f21884c58427400b6
Gabe Orroth <gorroth1@gmail.com>
2016-02-23 13:23:06 -0500

    save

--- js/play.js
+++ js/play.js
@@ -8,13 +8,20 @@ PingPong.play = () {};
 
 PingPong.play.prototype = {
 
-  boot: function() {
-  consol.log("Do you smell that!!!????");
-  };
-
 
   function init() {
+  
   };
 
+ function preload() {
+  
+    this.load.image('background','assets/Imported.piskel.png');
+    this.load.spritesheet('MOOK','assets/Imported.piskel.png',46,64,2);
+
+ };   
   function create() {
+  
+s.background = this.add.tileSprite(0,0,320,568,'background');
+      this.background.autoScroll(0,50);
+      this.background.scale.set(1);
   };
