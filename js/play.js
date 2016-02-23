bd3f9789ec3c86862c89f7d81f6d6980a14e1ec5
Gabe Orroth <gorroth1@gmail.com>
2016-02-23 10:15:56 -0500

    save

---
+++ js/play.js
@@ -0,0 +1,20 @@
+!#/usr/bin/env node
+
+'use strict'
+
+var PingPong = {};
+
+PingPong.play = () {};
+
+PingPong.play.prototype = {
+
+  boot: function() {
+  consol.log("Do you smell that!!!????");
+  };
+
+
+  function init() {
+  };
+
+  function create() {
+  };
